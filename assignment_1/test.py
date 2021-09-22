b5e548b358807724a539286849ff1de67ba783cc
Erik Alexander Sandvik <erikasan@student.matnat.uio.no>
2021-09-22 14:02:25 +0200

    Saving work

--- assignment_1/test.py
+++ assignment_1/test.py
@@ -1,6 +1,39 @@
+import numpy as np
 import pandas as pd
 
-index = pd.read_table('adult.names', skiprows=97)
+features = []
+output = ''
 
-df = pd.read_table('adult.data')
-#df.set_index(index)
+with open('adult.names') as infile:
+    info = infile.readlines()
+    output = info[94]
+    for line in info[96:]:
+        words = line.split()
+        features.append(words[0])
+
+
+for i in range(len(features)):
+    features[i] = features[i][:-1]
+
+print("Feature names:\n", features, '\n')
+print("Output name:\n", output)
+
+df = pd.read_csv('adult.data')
+df.columns = features + [output]
+
+df[['age']].mean()
+
+def get_info(feature):
+    mean = df[[feature]].mean()
+    std = df[[feature]].std()
+    median = df[[feature]].median()
+    q3, q1 = np.percentile(df[[feature]], [75 ,25])
+    max = df[[feature]].max()
+    min = df[[feature]].min()
+    print('mean: ', mean)
+    print('std: ', std)
+    print('median: ', median)
+    print('q1: ', q1)
+    print('q3: ', q3)
+    print('min: ', min)
+    print('max: ', max)
